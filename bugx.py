cd9d8090a1a3f0454c7f677ae218a9dd6ebd3853
ToncoBalonco <124464282+ToncoBalonco@users.noreply.github.com>
2024-05-27 21:17:53 +0200

    bugx.py

--- bugx.py
+++ bugx.py
@@ -17,18 +17,18 @@ class Bug2(Node):
         self.bug_algorithm_timer = self.create_timer(0.1, self.bug_algorithm_callback)
         self.goal_x = None
         self.goal_y = None
-        self.current_x = 0.0
-        self.current_y = 0.0
-        self.current_theta = 0.0
-        self.fl_sensor_value = 0.0
-        self.fr_sensor_value = 0.0
-        self.cmd_vel_msg = Twist()
-        self.state = 'find_direction'
         self.hit_point = None
         self.leave_point = None
         self.initial_position = None
         self.final_position = None
         self.direction_found = False
+        self.cmd_vel_msg = Twist()
+        self.current_x = 0.0
+        self.current_y = 0.0
+        self.current_theta = 0.0
+        self.fl_sensor_value = 0.0
+        self.fr_sensor_value = 0.0
+        self.state = 'find_direction'
 
     def goal_callback(self, msg):
         self.goal_x = msg.pose.position.x
@@ -48,33 +48,27 @@ class Bug2(Node):
         self.current_theta = transform.euler_from_quaternion(q)[2]  # [-pi, pi]
         if self.initial_position is None:
             self.initial_position = self.current_position
-
     def fl_sensor_callback(self, msg):
-        self.fl_sensor_value = msg.range / 0.65  # Povećana vrijednost senzora
+        self.fl_sensor_value = msg.range / 0.5  
         if self.fl_sensor_value < 0.0:
             self.fl_sensor_value = 0.0
-
     def fr_sensor_callback(self, msg):
-        self.fr_sensor_value = msg.range / 0.65  # Povećana vrijednost senzora
+        self.fr_sensor_value = msg.range / 0.5 
         if self.fr_sensor_value < 0.0:
             self.fr_sensor_value = 0.0
-
     def calculate_euclidean_distance(self, x1, y1, x2, y2):
         return math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
-
     def is_on_line(self):
         goal_vec = [self.goal_x - 0.0, self.goal_y - 0.0]
         pos_vec = [self.current_x - 0.0, self.current_y - 0.0]
         cross_product = goal_vec[0] * pos_vec[1] - goal_vec[1] * pos_vec[0]
-        return abs(cross_product) < 0.01  # Smanjen prag za detekciju odstupanja
-
+        return abs(cross_product) < 0.01  
     def bug_algorithm_callback(self):
         if self.goal_x is None or self.goal_y is None:
             self.cmd_vel_msg.linear.x = 0.0
             self.cmd_vel_msg.angular.z = 0.0
             self.cmd_pub.publish(self.cmd_vel_msg)
             return
-
         if self.state == 'find_direction':
             goal_angle = math.atan2(self.goal_y - self.current_y, self.goal_x - self.current_x)
             angle_diff = goal_angle - self.current_theta
@@ -87,14 +81,13 @@ class Bug2(Node):
             if abs(angle_diff) < 0.05:
                 self.direction_found = True
                 self.state = 'move_to_goal'
-
         elif self.state == 'move_to_goal':
             if not self.direction_found:
                 return
             if self.fl_sensor_value < 0.65 or self.fr_sensor_value < 0.65:
                 self.hit_point = (self.current_x, self.current_y)
                 self.state = 'follow_boundary'
-                self.cmd_vel_msg.angular.z = 0.1  # Turn left
+                self.cmd_vel_msg.angular.z = 0.1  # Turn soft left
                 self.cmd_vel_msg.linear.x = 0.0
             else:
                 goal_angle = math.atan2(self.goal_y - self.current_y, self.goal_x - self.current_x)
@@ -103,17 +96,14 @@ class Bug2(Node):
                     angle_diff -= 2 * math.pi
                 elif angle_diff < -math.pi:
                     angle_diff += 2 * math.pi
-                self.cmd_vel_msg.angular.z = 0.3 * angle_diff  # Smanjena kutna brzina
-                self.cmd_vel_msg.linear.x = 0.1  # Smanjena linearna brzina
-
-
+                self.cmd_vel_msg.angular.z = 0.3 * angle_diff  
+                self.cmd_vel_msg.linear.x = 0.1              
         elif self.state == 'follow_boundary':
             if self.fl_sensor_value > 0.65 and self.fr_sensor_value > 0.65:
                 self.state = 'move_to_goal'
                 self.leave_point = (self.current_x, self.current_y)
                 self.cmd_vel_msg.angular.z = 0.0
                 self.cmd_vel_msg.linear.x = 0.1
-
         elif self.state == 'follow_boundary':
             if self.fl_sensor_value > 0.65 and self.fr_sensor_value > 0.65:
                 self.state = 'move_to_goal'
@@ -127,9 +117,7 @@ class Bug2(Node):
                     self.state = 'move_to_goal'
                     self.cmd_vel_msg.angular.z = 0.0
                     self.cmd_vel_msg.linear.x = 0.0
-
         self.cmd_pub.publish(self.cmd_vel_msg)
-
 def main(args=None):
     rclpy.init(args=args)
     bug_node = Bug2()
